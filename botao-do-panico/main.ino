d11b456f8d11b7fc87df4e0a0d05f92f7275ac1f
Eduardo Henrique Freire Machado <62185704+ed-henrique@users.noreply.github.com>
2024-11-30 10:22:04 -0400

    Update main.ino

--- botao-do-panico/main.ino
+++ botao-do-panico/main.ino
@@ -2,13 +2,13 @@
 #include <HTTPClient.h>
 #include <UrlEncode.h>
 
-#define LED 23
 #define BUTTON 21
-#define WIFI_SSID "CIT_Alunos"
-#define WIFI_SENHA "alunos@2024"
+#define LED_BUZZER 23
+#define WIFI_SSID "INSIRA_O_NOME_DO_WIFI_AQUI" // IMPORTANTE
+#define WIFI_SENHA "INSIRA_A_SENHA_DO_WIFI_AQUI" // IMPORTANTE
 
-String TELEFONE = "559591258388";
-String CALLMEBOT_API_KEY = "4882014";
+String TELEFONE = "INSIRA_SEU_TELEFONE_AQUI"; // IMPORTANTE
+String CALLMEBOT_API_KEY = "INSIRA_SUA_CHAVE_DO_CALLMEBOT_AQUI"; // IMPORTANTE
 
 void sendMessage(String message) {
   String url = "https://api.callmebot.com/whatsapp.php?phone=" + TELEFONE + "&apikey=" + CALLMEBOT_API_KEY + "&text=" + urlEncode(message);
@@ -30,7 +30,7 @@ void setup() {
   Serial.begin(115200);
 
   pinMode(BUTTON, INPUT_PULLUP);
-  pinMode(LED, OUTPUT);
+  pinMode(LED_BUZZER, OUTPUT);
 
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
@@ -46,9 +46,10 @@ void setup() {
 
 void loop() {
   if (digitalRead(BUTTON)) {
-    digitalWrite(LED, LOW);
+    digitalWrite(LED_BUZZER, LOW);
   } else {
-    digitalWrite(LED, HIGH);
+    digitalWrite(LED_BUZZER, HIGH);
     sendMessage("Socorro");
+    Serial.print("O pedido de socorro foi enviado e os dispositivos de alerta foram acionados.");
   }
 }
